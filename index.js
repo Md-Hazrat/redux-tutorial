303bc31f3cb4af4ded4eff0f8ba4c3512d0025a2
Md-Hazrat <mdhazratali8866@gmail.com>
2024-10-17 13:09:16 +0600

    working on store

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const INCRENENT = "INCREMENT";
+const { createStore } = require("redux");
+//defining constant
+const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
 const ADD_USER = "ADD_USER";
-
 
 // state
 
@@ -25,14 +26,49 @@ const incrementCounter = () => {
 };
 
 const decrementCounter = () => {
-    return {
-      type: DECREMENT,
-    };
+  return {
+    type: DECREMENT,
   };
-  
-  const addUser = () => {
+};
+
+/*  const addUser = () => {
     return {
       type: ADD_USER,
       payload: {name: "Ali"}
     };
-  };
+  }; */
+
+//create reducer for counter
+
+const counterReducer = (state = initialCounterState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      state;
+  }
+};
+
+//1. state
+//2. dispatch action
+//3. reducer
+//4. store - getState(), dispatch(), subscribe()
+
+//create store
+
+const store = createStore(counterReducer);
+
+store.subscribe(() => [console.log(store.getState)]);
+
+//dispatch action
+
+store.dispatch(incrementCounter());
